e3fec18efe92af4fbd91ed921b8a973dd8849277
Maciej Matuszewski <maciek.matuszewski@gmail.com>
2018-04-17 23:54:43 +0200

    Fix config.yml

--- src/templates/blog-post.template.js
+++ src/templates/blog-post.template.js
@@ -1,9 +1,28 @@
 import React from 'react'
+import Box from '../components/Box';
 
-function BlogPost() {
+function BlogPost({ data: { markdownRemark } }) {
   return (
-    <p>Blog Post</p>
+    <Box>
+      <h1>{markdownRemark.frontmatter.title}</h1>
+      <Box html>{markdownRemark.html}</Box>
+    </Box>
   )
 }
+
+export const pageQuery = graphql`
+  query BlogPostByID($id: String!) {
+    markdownRemark(id: { eq: $id }) {
+      id
+      html
+      frontmatter {
+        date(formatString: "MMMM DD, YYYY")
+        title
+        description
+        tags
+      }
+    }
+  }
+`
 
 export default BlogPost
