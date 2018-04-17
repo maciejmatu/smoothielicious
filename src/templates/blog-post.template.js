11de0756d082372ff84984a4aa7edd61024f5124
Maciej Matuszewski <maciek.matuszewski@gmail.com>
2018-04-18 00:34:49 +0200

    Fix js errors

--- src/templates/blog-post.template.js
+++ src/templates/blog-post.template.js
@@ -18,8 +18,6 @@ export const pageQuery = graphql`
       frontmatter {
         date(formatString: "MMMM DD, YYYY")
         title
-        description
-        tags
       }
     }
   }
--- src/templates/index-page.template.js
+++ src/templates/index-page.template.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import Link from 'gatsby-link'
 
 class IndexPage extends Component {
   render() {
