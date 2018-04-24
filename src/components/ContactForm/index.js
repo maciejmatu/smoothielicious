3a33b8a155e4062a8c28215328cc2d30a3caa512
Maciej Matuszewski <maciek.matuszewski@gmail.com>
2018-04-25 01:39:24 +0200

    Allow netlify to parse form before rendering app

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './style.scss';
 
-function ContactForm() {
+function ContactForm(props) {
   return (
-    <form className="Form" name="contact" method="POST" netlify="true">
+    <form className="Form" name="contact" method="POST" data-netlify="true" {...props}>
       <label className="FormItem">
         <p className="FormItem__title">Your Name:</p>
         <input className="FormItem__input" type="text" name="name" />
@@ -18,6 +18,8 @@ function ContactForm() {
         <p className="FormItem__title">Your Message:</p>
         <textarea className="FormItem__input" name="message" />
       </label>
+
+      <div className="FormItem__recaptcha" data-netlify-recaptcha></div>
 
       <button className="Form__button" type="submit">Send</button>
     </form>
