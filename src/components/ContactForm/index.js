e3a5087ae3a0fad12fc93445992988b3caabb782
Maciej Matuszewski <maciek.matuszewski@gmail.com>
2018-04-25 17:10:03 +0200

    Send for with ajax

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import { navigateTo } from "gatsby-link";
 import './style.scss';
+
+function handleSubmit(e) {
+  fetch("/", {
+    method: "POST",
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: new FormData(e.target)
+  })
+    .then(() => navigateTo('/success/'))
+    .catch(error => alert(error));
+
+  e.preventDefault();
+};
 
 function ContactForm(props) {
   return (
-    <form className="Form" name="contact" action="/success/" method="POST" data-netlify="true" {...props}>
+    <form onSubmit={this.handleSubmit} className="Form" name="contact" action="/success/" method="POST" data-netlify="true" {...props}>
+      <label hidden>
+        Don’t fill this out: <input name="bot-field" />
+      </label>
       <label className="FormItem">
         <p className="FormItem__title">Your Name:</p>
         <input className="FormItem__input" type="text" name="name" />
