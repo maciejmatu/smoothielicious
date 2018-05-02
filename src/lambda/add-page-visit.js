dea56908bbb67d1142b693e8d45a4147183dd0ef
Maciej Matuszewski <maciek.matuszewski@gmail.com>
2018-05-02 11:02:09 +0200

    Close db connection after record update

--- src/lambda/add-page-visit.js
+++ src/lambda/add-page-visit.js
@@ -3,41 +3,49 @@ const { MongoClient } = require('mongodb');
 const DB_URL = process.env.DB_URL || 'mongodb://localhost:27017';
 const DB_NAME = 'serverless-smoothielicious';
 
-function addPageVisit() {
-  return MongoClient.connect(`${DB_URL}/${DB_NAME}`)
-    .then((client) => {
-      const db = client.db(DB_NAME);
+function errorResponse(callback, err) {
+  console.error(err);
 
-      return db.collection('info')
-        .findOneAndUpdate({}, {
-          $inc: { requests: 1 }
-        }, {
-          projection: { _id: 0 },
-          upsert: true,
-          returnOriginal: false
-        }
-      )
-    })
+  callback(null, {
+    statusCode: 500,
+    body: JSON.stringify({ error: err })
+  })
 }
 
+function successResponse(callback, res) {
+  console.log('Saved new page request. Current count:', res.value.requests);
+
+  callback(null, {
+    statusCode: 200,
+    body: JSON.stringify(res)
+  });
+}
 
 exports.handler = function(event, context, callback) {
-  addPageVisit()
-    .then((res) => {
-      console.log('Saved new page request. Current count:', res.value.requests);
+  MongoClient.connect(`${DB_URL}/${DB_NAME}`, (err, connection) => {
+    if (err) return errorResponse(callback, err);
+
+    const db = connection.db(DB_NAME);
+    const infoCollection = db.collection('info');
+    const updateAction = { $inc: { requests: 1 } };
+    const updateOptions = {
+      projection: { _id: 0 },
+      upsert: true,
+      returnOriginal: false
+    };
+
+    infoCollection.findOneAndUpdate({}, updateAction, updateOptions, (err, result) => {
+      if (err) return errorResponse(callback, err);
+
+      console.log('Saved new page request. Current count:', result.value.requests);
+
+      connection.close();
 
       callback(null, {
         statusCode: 200,
-        body: JSON.stringify(res)
+        body: JSON.stringify(result)
       });
     })
-    .catch((err) => {
-      console.error(err);
-
-      callback(null, {
-        statusCode: 500,
-        body: JSON.stringify({ errors: ['No db']})
-      })
-    });
+  });
 }
 
