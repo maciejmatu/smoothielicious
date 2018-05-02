60c9abd4b52daca7b4059ff875890208197d109b
Maciej Matuszewski <maciek.matuszewski@gmail.com>
2018-05-03 01:08:26 +0200

    Use the same module system across files

--- src/lambda/modules/mail-template.js
+++ src/lambda/modules/mail-template.js
@@ -1,4 +1,4 @@
-export default ({ name }) => {
+module.exports = ({ name }) => {
   return `
     <!DOCTYPE html>
     <html xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office">
